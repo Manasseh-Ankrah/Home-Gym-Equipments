b590eff1b3fc30638a0bc867bb011337b508850d
Manasseh-Ankrah <solomonankrah24@gmail.com>
2022-04-16 18:18:56 +0000

    fifth commit

--- server.php
+++ server.php
@@ -70,17 +70,64 @@ if (isset($_POST['create'])) {
 
 
 
-// Count
+// Fetch User Data
 
-$fetch_user = "SELECT id, firstname, lastname FROM users";
+$fetch_user = "SELECT id, firstname, lastname, user_name, phonenumber FROM users";
 $result = $conn->query($fetch_user);
+
+$userId;
 
 if ($result->num_rows > 0) {
     // output data of each row
     while ($row = $result->fetch_assoc()) {
+        $fName = $row["firstname"];
+        $lName = $row["lastname"];
+        $username = $row["user_name"];
+        $phone = $row["phonenumber"];
+        $userId = $row["id"];
         // echo "id: " . $row["id"] . " - Name: " . $row["firstname"] . " " . $row["lastname"] . "<br>";
     }
 } else {
     echo "0 results";
 }
+
+
+
+
+
+// Update User
+
+// $id = $_GET['updateId'];
+// $updateSql = "select * from users where id=$userId";
+// $updateResult = mysqli_query($conn, $updateSql);
+
+// if ($updateResult) {
+//     $updateRow = mysqli_fetch_assoc($updateResult);
+//     $update_fName = $updateRow['firstname'];
+//     $update_lName = $updateRow['lastname'];
+//     $update_username = $updateRow['user_name'];
+//     $update_phone = $updateRow['phonenumber'];
+// }
+
+// if (isset($_POST['submit'])) {
+//     $firstname  = $_POST['firstname'];
+//     $lastname   = $_POST['lastname'];
+//     $username   = $_POST['user_name'];
+//     $phonenumber = $_POST['phonenumber'];
+
+//     // Insert into users (firstname, lastname, user_name, email,postcode, phonenumber, user_password,display_name)
+
+//     $sql = "update users set id=$id,firstname='$firstname',lastname='$lastname',user_name='$user_name' where id=$userId";
+//     $result = mysqli_query($conn, $sql);
+
+//     if ($result) {
+//         header("Location:index.php");
+//         // echo 'updated successfully';
+//     } else {
+//         die(mysqli_error($conn));
+//     }
+// }
+
+
+
 $conn->close();
---
+++ viewProfile.php
@@ -0,0 +1,192 @@
+<?php
+include 'server.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/5f87064e2901b920769365bd/default';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/fontawesome/css/all.css" />
+    <link href="css/style.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <!-- <i class="fab fa-github fa-2x mx-3 ps-1"></i> -->
+                <h3 class="p-1">HGE</h3>
+            </a>
+            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <form class="me-3">
+                    <div class="form-white input-group" style="width: 250px;">
+                        <input type="search" class="form-control rounded" placeholder="Search or jump to..." aria-label="Search" aria-describedby="search-addon" />
+                    </div>
+                </form>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="information.php">Information</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="wanted.php">Wanted</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="workshop.php">Workshop</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="gallery.php">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="featured.php">Featured</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav d-flex flex-row ms-auto me-3">
+                    <li class="nav-item me-3 me-lg-0 dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
+                            <!-- <a href="#">Account</a> -->
+                            <i class="fas fa-plus"></i>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider" />
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <section>
+        <div class="profile-text text-center mt-4">
+            <h1>View Profile</h1>
+        </div>
+    </section>
+
+    <div class="container mt-5 mb-5">
+        <div class="row">
+            <div class="col-md-6 user-icon-div">
+                <i class="bi bi-instagram user-profile-icon"></i>
+
+            </div>
+            <div class="col-md-6">
+                <div>
+                    <form action="" method="post">
+                        <div class="container">
+                            <div class="row register-row">
+                                <div class="col-sm-12">
+                                    <label for="firstname"><b>First Name</b></label>
+                                    <input class="form-control" disabled id="firstname" type="text" value="<?php echo $fName ?>" name="firstname" required>
+
+                                    <label for="lastname"><b>Last Name</b></label>
+                                    <input class="form-control" disabled id="lastname" type="text" value="<?php echo $lName ?>" name="lastname" required>
+
+                                    <label for="username"><b>Username</b></label>
+                                    <input class="form-control" disabled id="username" type="text" value="<?php echo $username ?>" name="user_name" required>
+
+                                    <label for="phonenumber"><b>Phone Number</b></label>
+                                    <input class="form-control" disabled id="phonenumber" type="text" value="<?php echo $phone ?>" name="phonenumber" required>
+
+
+                                    <hr class="mb-3">
+                                    <!-- <div style="display: flex;justify-content:center;align-items:center;">
+                                        <input type="submit" class="btn btn-primary" class="header-text" id="register" name="create" value="Sign Up">
+                                    </div> -->
+
+                                    <!-- <button class="btn btn-primary"><a href="wanted.php" class="text-light">Register</a></button> -->
+                                    <!-- <P style="color: white;display: flex;justify-content:center;margin-top:10px">Already have an account? ...<a href="login.php"> Login</a></P> -->
+
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <footer class="bg-dark text-center text-white">
+        <!-- Grid container -->
+        <div class="container text-center p-4 pb-0">
+            <!-- Section: Social media -->
+            <section class="mb-4 footer-icons">
+                <!-- Facebook -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+
+                <!-- Twitter -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+
+                <!-- Google -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
+
+                <!-- Instagram -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+
+                <!-- Linkedin -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+
+                <!-- Github -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-github"></i></a>
+            </section>
+            <!-- Section: Social media -->
+        </div>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2020 Copyright:
+            <a class="text-white" href="https://mdbootstrap.com/">HGE</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
